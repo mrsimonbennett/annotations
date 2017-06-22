18ff627e403afd5e18e1942e41731c706b33ffb0
Simon Bennett <simon@bennett.im>
2017-06-22 16:06:20 +0000

    Apply fixes from StyleCI

--- tests/Routing/RoutingAnnotationScannerTest.php
+++ tests/Routing/RoutingAnnotationScannerTest.php
@@ -2,15 +2,14 @@
 
 use Collective\Annotations\Routing\Annotations\Scanner;
 
-class RoutingAnnotationScannerTest extends PHPUnit_Framework_TestCase {
+class RoutingAnnotationScannerTest extends PHPUnit_Framework_TestCase
+{
+    public function testProperRouteDefinitionsAreGenerated()
+    {
+        require_once __DIR__.'/fixtures/annotations/BasicController.php';
+        $scanner = Scanner::create(['App\Http\Controllers\BasicController']);
+        $definition = str_replace(PHP_EOL, "\n", $scanner->getRouteDefinitions());
 
-	public function testProperRouteDefinitionsAreGenerated()
-	{
-		require_once __DIR__.'/fixtures/annotations/BasicController.php';
-		$scanner = Scanner::create(['App\Http\Controllers\BasicController']);
-		$definition = str_replace(PHP_EOL, "\n", $scanner->getRouteDefinitions());
-
-		$this->assertEquals(trim(file_get_contents(__DIR__.'/results/annotation-basic.php')), $definition);
-	}
-
+        $this->assertEquals(trim(file_get_contents(__DIR__.'/results/annotation-basic.php')), $definition);
+    }
 }
